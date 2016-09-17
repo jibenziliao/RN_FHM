410b4de7be9093c6c41a44a74ccdee7753bf90f4
jibenziliao <1501718947@qq.com>
2016-09-17 16:21:08 +0800

    调整部分代码。

--- App/Services/Api.js
+++ App/Services/Api.js
@@ -18,18 +18,18 @@ const create = (baseURL = 'http://api.openweathermap.org/data/2.5/') => {
     },
     // 10 second timeout...
     timeout: 10000
-  })
+  });
 
   // Force OpenWeather API Key on all requests
   api.addRequestTransform(request => {
     request.params['APPID'] = '0e44183e8d1018fc92eb3307d885379c'
-  })
+  });
 
   // Wrap api's addMonitor to allow the calling code to attach
   // additional monitors in the future.  But only in __DEV__ and only
   // if we've attached Reactotron to console (it isn't during unit tests).
   if (__DEV__ && console.tron) {
-    console.tron.log('Hello, I\'m an example of how to log via Reactotron.')
+    console.tron.log('Hello, I\'m an example of how to log via Reactotron.');
     api.addMonitor(console.tron.apisauce)
   }
 
@@ -65,7 +65,7 @@ const create = (baseURL = 'http://api.openweathermap.org/data/2.5/') => {
     // a list of the API functions from step 2
     getCity
   }
-}
+};
 
 // let's return back our create method as the default.
 export default {
